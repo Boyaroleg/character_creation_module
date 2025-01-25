94725e40d07722bae9149e5a8f682d1037d86761
Олег Боярков <boyaroleg92@gmail.com>
2025-01-25 16:10:02 +0500

    main name constructios

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -92,6 +92,4 @@ def main() -> None:
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
-
-main()
+    print(start_training(char_name, char_class)) 
