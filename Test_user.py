14936cd8b650b0e24e9e4e55db0b1b927f992263
Олег Боярков <boyaroleg92@gmail.com>
2025-01-28 13:37:57 +0500

    opisanie funccii

--- Test_user.py
+++ Test_user.py
@@ -1,8 +1,20 @@
-import math
+from math import sqrt
 
-# Спросим, что хорошего в этой библиотеке.
-print(math.__doc__)
-print(print.__doc__)
-# Будет напечатано:
-# This module provides access to the mathematical functions
-# defined by the C standard. 
+message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
+          'квадратного корня из заданного числа'
+print(message)
+
+
+def CalculateSquareRoot(Number):
+    """Вычисляет квадратный корень."""
+    return sqrt(Number)
+
+
+def calc(your_number):
+    """Вычисляет квадратный корень из введенного вами числа."""
+    if your_number <= 0:
+        print('Пожалуйста, введите положительное число.')
+        return
+    result = CalculateSquareRoot(your_number)
+    print(f'Мы вычислили квадратный корень из введённого вами числа.'
+          f'Это будет: {result}')
